5f0008640a54159e4c6885e2027334effa0a43a3
lamhuy <huylam06@gmail.com>
2017-10-24 21:28:02 -0400

    create and appending json dynamically

--- upload_s3.py
+++ upload_s3.py
@@ -5,6 +5,7 @@ import sys
 import json
 import urllib.parse
 import boto3
+import botocore
 
 from queue import Queue
 from threading import Thread
@@ -53,8 +54,8 @@ if __name__ == "__main__":
     
     TRACKS_FILE_NAME = args.tracks
     PATH_MP3 = args.path  
-    ALBUM = args.album
-    ARTIST = args.artist       
+    ALBUM_NAME = args.album
+    ARTIST_NAME = args.artist       
     ARTIST_KEY = args.akey
     ALBUM_KEY = args.bkey   
     BUCKET_NAME = args.bucket    
@@ -72,12 +73,9 @@ if __name__ == "__main__":
     
     #tracks_titles=['Đường Xưa Mây Trắng'.encode('utf8'), 'Dogs Eating Dogs', 'Disaster', 'END']
     #tracks_titles=['abc', 'Dogs Eating Dogs', 'Disaster', 'END']
-
-    json = '{ \n\t"title" : "'
-    json += ALBUM
-    json += '",\n'
-    json += '\t"playlist": [ { \n'
-    
+    json = {}
+    json["title"] = ALBUM_NAME
+    json["playlist"] = []
     for i, track in enumerate(tracks_titles):
       #  track = track.decode('utf-8')
         if(i == len(tracks_titles)-1):
@@ -85,14 +83,13 @@ if __name__ == "__main__":
             
         #print(i)
         #print(len(tracks_titles))
-        json += '\t\t"title": "' + track + '",\n'
-        if ARTIST:
-            json += '\t\t"artist": "' + ARTIST + '",\n'
-        json += '\t\t"src": "' + S3_BUCKET + BUCKET_PATH + track + '.mp3"\n'        
-        if(i < len(tracks_titles)-2):         
-            json += '\t}, { \n'
-    
-    json += '\t} ] \n }'
+        data = {}
+        data["title"] = track
+        if ARTIST_NAME:
+            data["artist"] = ARTIST_NAME
+        data["src"] = S3_BUCKET _ BUCKET_PATH + track + '.mp3'
+        
+        json["playlist"].append(data)
     print(json) 
     
     print('Uploading mp3 tracks')
@@ -108,12 +105,93 @@ if __name__ == "__main__":
     
     #upload playlist into aws 
     print('Uploading playlist')
-    filename = 'playlist_' + ALBUM_KEY + '.json'  
+    PLAYLIST_FILE = 'playlist_' + ALBUM_KEY + '.json'  
     
     # Uploads the given file using a managed uploader, which will split up large
     # files automatically and upload parts in parallel.
-    s3.Object(BUCKET_NAME, BUCKET_PATH +filename).put(Body=json)
+    s3.Object(BUCKET_NAME, BUCKET_PATH +PLAYLIST_FILE).put(Body=json)
     
-    #download dhramaCast_ARTIST_KEY.json, append this playlist info, then upload
     
+    
+    #download dhramaCast_ARTIST_KEY.json, append this dharmaCast_ARTIST_KEY.json info, then upload
+    ARTIST_JSON_FILE = ARTIST_KEY + '/dharmaCast_'+ARTIST_KEY +'.json'
+    S3_BUCKET = 'http://'+BUCKET_NAME+'.s3-website-us-east-1.amazonaws.com/'
+    s3 = boto3.resource('s3')
+
+
+    #adding album into into artist dharma cast list
+    try:
+        artist_json = s3.Object(BUCKET_NAME, ARTIST_JSON_FILE).get()["Body"]
+        artist_json = json.load(artist_json)
+    except botocore.exceptions.ClientError as e:
+        print(e.response['Error']['Code'])
+        if e.response['Error']['Code'] == "NoSuchKey":
+            print("The object does not exist. creating ARTIST_JSON_FILE")
+            artist_json = {}
+            artist_json['title'] = ARTIST_NAME
+            data = {}
+            data['title'] = ALBUM_NAME
+            data['listName'] = ALBUM_KEY
+            data['id'] = 1
+            artist_json['playlists'] = []
+            artist_json['playlists'].append(data)
+        else:
+            raise
+
+    print(artist_json)
+    print(artist_json["playlists"])
+    albumExist = False
+
+    for i, playlist in enumerate(artist_json["playlists"]):
+        print(playlist["listName"])
+        if(ARTIST_KEY == playlist["listName"]):
+            albumExist = True
+            break;
+
+
+    if(not albumExist):
+        print("adding album to artist_json")
+        #build json object, append it
+        data = {}
+        data['title'] = ALBUM_NAME
+        data['listName'] = ALBUM_KEY
+        data['id'] = len(artist_json["playlists"])+1
+        artist_json["playlists"].append(data)
+        print(artist_json["playlists"])
+        #upload new file into S3
+        #s3.Object(BUCKET_NAME, ARTIST_JSON_FILE).put(Body=artist_json)
+
+    else:
+        print("ALBUM EXIST")
+
+
+    #adding artist into dharma cast.json list
+    DHARMA_JSON_FILE = 'dharmaCast.json'
+    dharma_json = s3.Object(BUCKET_NAME, DHARMA_JSON_FILE).get()["Body"]
+    artistExist = False
+
+    dharma_json = json.load(dharma_json)
+    print(dharma_json)
+
+    for i, artist in enumerate(dharma_json):
+        print(artist["listName"])
+        if(ARTIST_KEY == artist["listName"]):
+            artistExist = True
+            break;
+
+
+    if(not artistExist):
+        print("adding artist to dharma_json")
+        #build json object, append it
+        data = {}
+        data['title'] = ARTIST_NAME
+        data['listName'] = ARTIST_KEY
+        data['id'] = len(dharma_json)+1
+        dharma_json.append(data)
+        print(dharma_json)
+        #upload new file into S3
+        #s3.Object(BUCKET_NAME, ARTIST_JSON_FILE).put(Body=artist_json)
+
+    else:
+        print("ARTIST EXIST")
     
